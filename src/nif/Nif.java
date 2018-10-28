f969103569e2df8fe2fa1ae5da4d4dc996cfa453
user <user@192.168.1.34>
2018-10-28 12:15:19 +0100

    Añadido metodo calcularLetra

--- src/nif/Nif.java
+++ src/nif/Nif.java
@@ -21,13 +21,24 @@ public class Nif {
         return tipoDocumento;
     }
     
+   /**
+    * Esta función devuelve dado un número de DNI la letra que le corresponde,
+    * asigna el resto de dividir el número de dni por 23 y el resultado es una
+    * posición de una tabla donde le corresponde la letra.
+    */    
     public static char calcularLetra (int numero){
+ 
+        char letra;
+        String letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE"; /* Creo string donde
+        recreeo la tabla de equivalencias, para una vez tenga el resultado del
+        resto, será la posición de la letra con el método charAt.
+        */
+        numero = numero % 23;
         
-        char letra = 0;    
-    
-    
-    
+        letra = letrasDNI.charAt(numero);
+        
         return letra;
+    
     }
     
     public static boolean verificarLetra(int numero, char letra){
@@ -40,37 +51,37 @@ public class Nif {
         return check;
     
     }
-/**
- * Método tipoEmpresa que determina el tipo de Empresa en función del
- * Nif/Cif (para personas jurídicas). En las personas jurídicas
- * la primera posición del nif/cif es una letra que determina la 
- * naturaleza jurídica:  
- * <ul>
- *  <li> A -- Sociedades anónimas.</li> 
- *  <li> B -- Sociedades de responsabilidad limitada.</li> 
- *  <li> C -- Sociedades colectivas.</li> 
- *  <li> D -- Sociedades comanditarias.</li> 
- *  <li> E -- Comunidades de bienes.</li> 
- *  <li> F -- Sociedades cooperativas.</li> 
- *  <li> G -- Asociaciones y fundaciones.</li>
- *  <li> H -- Comunidades de propietarios en régimen de propiedad horizontal.</li>
- *  <li> J -- Sociedades civiles.</li>
- *  <li> N -- Entidades no residentes.</li>
- *  <li> P -- Corporaciones locales.</li>
- *  <li> Q -- Organismos autónomos, estatales o no, y asimilados.</li>
- *  <li> R -- Congregaciones e instituciones religiosas </li>
- *  <li> S -- Órganos de la Administración del Estado ycomunidades autónomas </li> 
- *  <li> U -- Uniones Temporales de Empresas.</li> 
- *  <li> V -- Sociedad Agraria de Transformación.</li>
- *  <li> W -- Establecimientos permanentes de entidades no residentes en España </li>
- * </ul>
- * @param idDocumento Es una cadena con el Nif/Cif a evaluar
- * @return            El tipo de entidad con el valor de la primera posicion de
- *                    la cadena idDocumento, si este se encuentra en la lista
- *                    de letras de nif correspondientes a personas jurídicas. 
- *                    En caso contrario, no es persona jurídica devuelve un espacio (' '). 
- */
     
+   /**
+    * Método que determina el tipo de Empresa en función del
+    * Nif/Cif (para personas jurídicas). En las personas jurídicas
+    * la primera posición del nif/cif es una letra que determina la 
+    * naturaleza jurídica:  
+    * <ul>
+    *  <li> A -- Sociedades anónimas.</li> 
+    *  <li> B -- Sociedades de responsabilidad limitada.</li> 
+    *  <li> C -- Sociedades colectivas.</li> 
+    *  <li> D -- Sociedades comanditarias.</li> 
+    *  <li> E -- Comunidades de bienes.</li> 
+    *  <li> F -- Sociedades cooperativas.</li> 
+    *  <li> G -- Asociaciones y fundaciones.</li>
+    *  <li> H -- Comunidades de propietarios en régimen de propiedad horizontal.</li>
+    *  <li> J -- Sociedades civiles.</li>
+    *  <li> N -- Entidades no residentes.</li>
+    *  <li> P -- Corporaciones locales.</li>
+    *  <li> Q -- Organismos autónomos, estatales o no, y asimilados.</li>
+    *  <li> R -- Congregaciones e instituciones religiosas </li>
+    *  <li> S -- Órganos de la Administración del Estado ycomunidades autónomas </li> 
+    *  <li> U -- Uniones Temporales de Empresas.</li> 
+    *  <li> V -- Sociedad Agraria de Transformación.</li>
+    *  <li> W -- Establecimientos permanentes de entidades no residentes en España </li>
+    * </ul>
+    * @param idDocumento Es una cadena con el Nif/Cif a evaluar
+    * @return            El tipo de entidad con el valor de la primera posicion de
+    *                    la cadena idDocumento, si este se encuentra en la lista
+    *                    de letras de nif correspondientes a personas jurídicas. 
+    *                    En caso contrario, no es persona jurídica devuelve un espacio (' '). 
+    */    
     public static char tipoEmpresa (String idDocumento){
         // variable de retorno
         char tipoEntidad = ' ';
